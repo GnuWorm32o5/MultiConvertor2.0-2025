bcd1c2a15fd0b8b9202a529138d8bd8aee65c092
GnuWorm32o5 <stevanglavaskii@gmail.com>
2025-09-22 19:41:13 +0200

    Project finished

--- app/src/main/java/com/example/multiconvertor20_2025/MainActivity.java
+++ app/src/main/java/com/example/multiconvertor20_2025/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.multiconvertor20_2025;
-import androidx.activity.OnBackPressedCallback;
-import android.content.Intent;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.Toast;
+import android.webkit.DownloadListener;
 import androidx.appcompat.app.AppCompatActivity;
+
 
 
 
@@ -25,6 +34,59 @@ public class MainActivity extends AppCompatActivity {
 
         webView.getSettings().setJavaScriptEnabled(true);   // dukljuci JS ako batre
         webView.setWebViewClient(new WebViewClient());  // kljuc svega, keeps nav inside app logicno
+
+
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimeType,
+                                        long contentLenght) {
+
+                try {
+                    //creates a request to download the file at the url
+                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+
+                    request.setMimeType(mimeType);
+                    // user-agent that some servers need
+
+                    request.addRequestHeader("User-Agent", userAgent);
+
+                    // get notifications
+                    request.setNotificationVisibility(
+                            DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+                    // downloading over wifi and data
+                    request.setAllowedNetworkTypes(
+                            DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+
+                    // guess filename if not specified
+                    String fileName = URLUtil.guessFileName(url, contentDisposition, mimeType);
+                    // destination for android 10+ and emulator
+
+                    request.setDestinationInExternalFilesDir(
+                            MainActivity.this, Environment.DIRECTORY_DOWNLOADS, fileName);
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+
+                    // use system download manager
+                    DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                    dm.enqueue(request);
+
+                    Toast.makeText(getApplicationContext(), "Downloading File...", Toast.LENGTH_SHORT).show();
+
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Donwload failed: ", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+        //runtime permission for android sdk 6.0+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
     }
 
 
